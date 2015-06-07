22931d331839677b7e5dc1cc8aad6fbcf29d567d
Kirk Northrop <kirk@krn.me.uk>
2015-06-07 10:02:19 +0100

    Add requirements, merge all the random versions

--- settings-default.py
+++ settings-default.py
@@ -1,4 +1,7 @@
 app_key = ''
 app_secret = ''
 template_file_location = ''
-output_file_location = ''
+output_file_location = '/public/'
+production = False
+tubelifts_oauth_token = ''
+tubelifts_oauth_token_secret = ''
