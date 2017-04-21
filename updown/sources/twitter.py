159be0580d2c594dbb7da5f94e59f1bd9eab6b4e
Kirk Northrop <kirk@krn.me.uk>
2017-04-21 21:15:39 +0100

    Move all the unused twitter stuff out of the way

--- updown/sources/twitter.py
+++ updown/sources/twitter.py
@@ -1,5 +1,75 @@
 EXPLICIT_RESOLVE = True
 
+###########################################
+# Lots of currently unused Twitter stuff. #
+###########################################
+
+
+# Get a twitter object
+def get_twitter():
+
+    if twitter is None:
+        try:
+            twitter = Twython(APP_KEY, access_token=get_twitter_access_token())
+            twitter.get_user_timeline(screen_name='TFLOfficial')
+        except TwythonAuthError:
+            twitter = Twython(APP_KEY, access_token=create_twitter_access_token())
+
+    return twitter
+
+
+# Do we need to update twitter again yet?
+def twitter_needs_update():
+
+    last_twitter = datetime.strptime(get_problems_dict()['_twitter_update'], '%Y-%m-%dT%H:%M:%S')
+
+    return last_twitter + timedelta(minutes=2) < datetime.now()
+
+
+# Convert the odd time that comes from twython to a sensible one
+def convert_tweet_time(time):
+    # Format: Fri Sep 13 15:23:01 +0000 2013 - python seems to dislike %z so we're using +0000
+    naive_tweet_time = datetime.strptime(time, '%a %b %d %H:%M:%S +0000 %Y')
+    utc_tweet_time = pytz.utc.localize(naive_tweet_time)
+    return utc_tweet_time.astimezone(timezone('Europe/London'))
+
+
+# Getter, Setter, Creator and Saver for twitter accounts and last statuses
+def get_twitter_last_statuses():
+
+    if twitter_last_statuses is None:
+        try:
+            with open(settings.template_file_location + 'twitter_laststatuses.json', 'r') as f:
+                twitter_last_statuses = json.loads(f.read())
+        except IOError:
+            twitter_last_statuses = {
+                'bakerlooline': 1,
+                'centralline': 1,
+                'circleline': 1,
+                'districtline': 1,
+                'hamandcityline': 1,
+                'jubileeline': 1,
+                'metline': 1,
+                'northernline': 1,
+                'piccadillyline': 1,
+                'victorialine': 1,
+                'wlooandcityline': 1,
+                'LondonDLR': 1,
+                'LDNOverground': 1,
+            }
+
+    return twitter_last_statuses
+
+
+def set_twitter_last_statuses(last_statuses):
+
+    twitter_last_statuses = last_statuses
+    with open(settings.template_file_location + 'twitter_laststatuses.json', 'w') as f:
+        f.write(json.dumps(last_statuses))
+
+    return True
+
+
 # Do everything we need to check twitter
 def check_twitter():
 
--- updown/stepfreeaccess.py
+++ updown/stepfreeaccess.py
@@ -29,46 +29,6 @@ SOURCE_RELIABILITY_ORDER = ['trackernet', 'tflapi', 'twitter']
 DEFAULT_EXCUSE = 'There are step free access issues at this station.'
 
 
-# Getter, Setter, Creator and Saver for twitter accounts and last statuses
-def get_twitter_last_statuses():
-
-    global twitter_last_statuses
-
-    if twitter_last_statuses is None:
-        try:
-            with open(settings.template_file_location + 'twitter_laststatuses.json', 'r') as f:
-                twitter_last_statuses = json.loads(f.read())
-        except IOError:
-            twitter_last_statuses = {
-                'bakerlooline': 1,
-                'centralline': 1,
-                'circleline': 1,
-                'districtline': 1,
-                'hamandcityline': 1,
-                'jubileeline': 1,
-                'metline': 1,
-                'northernline': 1,
-                'piccadillyline': 1,
-                'victorialine': 1,
-                'wlooandcityline': 1,
-                'LondonDLR': 1,
-                'LDNOverground': 1,
-            }
-
-    return twitter_last_statuses
-
-
-def set_twitter_last_statuses(last_statuses):
-
-    global twitter_last_statuses
-
-    twitter_last_statuses = last_statuses
-    with open(settings.template_file_location + 'twitter_laststatuses.json', 'w') as f:
-        f.write(json.dumps(last_statuses))
-
-    return True
-
-
 # Getter and Creator for twitter Access Token
 def create_twitter_access_token():
 
@@ -157,38 +117,6 @@ def set_problem_for_station(station, problem):
     set_problems_dict(problems)
 
     return True
-
-
-# Convert the odd time that comes from twython to a sensible one
-def convert_tweet_time(time):
-    # Format: Fri Sep 13 15:23:01 +0000 2013 - python seems to dislike %z so we're using +0000
-    naive_tweet_time = datetime.strptime(time, '%a %b %d %H:%M:%S +0000 %Y')
-    utc_tweet_time = pytz.utc.localize(naive_tweet_time)
-    return utc_tweet_time.astimezone(timezone('Europe/London'))
-
-
-# Get a twitter object
-def get_twitter():
-
-    global twitter
-
-    if twitter is None:
-        try:
-            twitter = Twython(APP_KEY, access_token=get_twitter_access_token())
-            twitter.get_user_timeline(screen_name='TFLOfficial')
-        except TwythonAuthError:
-            twitter = Twython(APP_KEY, access_token=create_twitter_access_token())
-
-    return twitter
-
-
-# Do we need to update twitter again yet?
-def twitter_needs_update():
-
-    last_twitter = datetime.strptime(get_problems_dict()['_twitter_update'], '%Y-%m-%dT%H:%M:%S')
-
-    return last_twitter + timedelta(minutes=2) < datetime.now()
-
 
 # Send a tweet
 def send_tweet(tweet_text):
