a5468ae116f10cb9dc77af7a410ce5031d031eee
Kirk Northrop <kirk@krn.me.uk>
2017-05-01 09:54:14 +0100

    Try and fix nulls

--- updown/utils.py
+++ updown/utils.py
@@ -50,6 +50,7 @@ def find_station_name(possible_name):
             return TFL_NAME_CORRECTIONS[station_name]
 
     print station_name, 'not found'
+    return possible_name
 
 
 def remove_tfl_specifics(text):
