808d97daf7de4514ab67fd55d2ad0ab3e6d2876f
Kirk Northrop <kirk@krn.me.uk>
2023-02-04 17:27:24 +0000

    Fix some env vars and add gunicorn

--- updown/updown/settings.py
+++ updown/updown/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h%m+eyvj3lc%=yrhv=qt&-y=v)m!*cwxsmzw0kn6b9i*pdhm55"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
