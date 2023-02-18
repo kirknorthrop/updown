de7f6de0e8f423bd0b044af820b240233daa9bc3
Kirk Northrop <kirk@krn.me.UK>
2023-02-18 12:18:16 +0000

    Make function generic

--- packages/updown/update_incidents/__main__.py
+++ packages/updown/update_incidents/__main__.py
@@ -5,9 +5,12 @@ import requests
 
 
 def main(args):
+    """
+    This can me the same for all functions
+    """
     url_base = os.getenv("FUNCTIONS_URL_BASE", "http://127.0.0.1:8000/functions/")
-    function_name = "update_incidents"
     secret_key = os.getenv("FUNCTIONS_SECRET_KEY", "verysecret")
+    function_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
     r = requests.post(urljoin(url_base, function_name), data={"key": secret_key})
 
