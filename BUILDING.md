859d5f5dd2c00fbccbeb12dc55b95845a69f45a0
Kirk Northrop <kirk@krn.me.UK>
2023-02-18 14:18:01 +0000

    Update function

---
+++ BUILDING.md
@@ -0,0 +1,4 @@
+# Building the trigger function
+```bash
+doctl serverless deploy updownlondon --env updownlondon/packages/updown/update_incidents/.env --remote-build
+```
--- packages/updown/update_incidents/__main__.py
+++ packages/updown/update_incidents/__main__.py
@@ -5,12 +5,9 @@ import requests
 
 
 def main(args):
-    """
-    This can me the same for all functions
-    """
     url_base = os.getenv("FUNCTIONS_URL_BASE", "http://127.0.0.1:8000/functions/")
     secret_key = os.getenv("FUNCTIONS_SECRET_KEY", "verysecret")
-    function_name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    function_name = "update_incidents"  # os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
     r = requests.post(urljoin(url_base, function_name), data={"key": secret_key})
 
