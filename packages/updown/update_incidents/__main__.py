50c52772f0733772b005c188b72c311abf7c4aca
Kirk Northrop <kirk@krn.me.UK>
2023-02-18 11:41:04 +0000

    Just send a request instead

--- packages/updown/update_incidents/__main__.py
+++ packages/updown/update_incidents/__main__.py
@@ -1,4 +1,14 @@
-from django.core.management import call_command
+import os
+from urllib.parse import urljoin
+
+import requests
+
 
 def main(args):
-    call_command('update_incidents')
+    url_base = os.getenv("FUNCTIONS_URL_BASE", "http://127.0.0.1:8000/functions/")
+    function_name = "update_incidents"
+    secret_key = os.getenv("FUNCTIONS_SECRET_KEY", "verysecret")
+
+    r = requests.post(urljoin(url_base, function_name), data={"key":secret_key})
+
+    r.raise_for_status()
--- packages/updown/update_incidents/build.sh
+++ packages/updown/update_incidents/build.sh
@@ -4,5 +4,5 @@ set -e
 
 virtualenv virtualenv
 source virtualenv/bin/activate
-pip install -r ../../../requirements.txt
+pip install -r requirements.txt
 deactivate
