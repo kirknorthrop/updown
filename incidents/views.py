bd62e05640c7bf6cfb8f366e656fc73284bfe100
Kirk Northrop <kirk@krn.me.uk>
2023-04-02 15:15:07 +0100

    Add alexa

--- incidents/views.py
+++ incidents/views.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from django.conf import settings
 from django.core.management import call_command
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
 from django.utils import timezone
 from django.utils.decorators import method_decorator
@@ -11,31 +11,59 @@ from django.views.decorators.csrf import csrf_exempt
 
 from incidents.models import Report
 
+ISSUES_QUERY = (
+    Report.objects.filter(resolved=False)
+    .order_by("station__parent_station__name")
+    .distinct("station__parent_station__name")
+)
+RESOLVED_QUERY = (
+    Report.objects.filter(
+        resolved=True, end_time__gte=timezone.now() - timedelta(hours=12)
+    )
+    .order_by("station__parent_station__name")
+    .distinct("station__parent_station__name")
+)
+INFORMATION_QUERY = (
+    Report.objects.filter(resolved=False, information=True)
+    .order_by("station__parent_station__name")
+    .distinct("station__parent_station__name")
+)
+
 
 def detail(request):
-    issues = (
-        Report.objects.filter(resolved=False)
-        .order_by("station__parent_station__name")
-        .distinct("station__parent_station__name")
-    )
-    resolved = (
-        Report.objects.filter(
-            resolved=True, end_time__gte=timezone.now() - timedelta(hours=12)
-        )
-        .order_by("station__parent_station__name")
-        .distinct("station__parent_station__name")
-    )
-    information = (
-        Report.objects.filter(resolved=False, information=True)
-        .order_by("station__parent_station__name")
-        .distinct("station__parent_station__name")
-    )
+    issues = ISSUES_QUERY
+    resolved = RESOLVED_QUERY
+    information = INFORMATION_QUERY
 
     return render(
         request,
         "home.html",
         {"issues": issues, "resolved": resolved, "information": information},
     )
+
+
+def alexa(request):
+    if ISSUES_QUERY.count() == 0:
+        alexa_string = "There are currently no reported step free access issues on the \
+            Transport for London network."
+    else:
+        alexa_string = "There are step free access issues at: "
+        alexa_string += ", ".join(
+            sorted(
+                ISSUES_QUERY.values_list("station__parent_station__name", flat=True)
+            )[0:-1]
+        )
+
+        if ISSUES_QUERY.count() > 1:
+            alexa_string += " and "
+
+        alexa_string += sorted(
+            ISSUES_QUERY.values_list("station__parent_station__name", flat=True)
+        )[-1]
+
+    alexa_string = alexa_string.replace("&", "and")
+
+    return HttpResponse(alexa_string)
 
 
 @method_decorator(csrf_exempt, name="dispatch")
--- updown/urls.py
+++ updown/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from incidents.views import detail, UpdateIncidentsView
+from incidents.views import detail, UpdateIncidentsView, alexa
 from pages.views import FAQPageView, PressPageView, PrivacyPageView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", detail, name="home"),
+    path("problems.txt", alexa),
     path("functions/update_incidents", UpdateIncidentsView.as_view()),
     path("faq/", FAQPageView.as_view(), name="faq"),
     path("press/", PressPageView.as_view(), name="press"),
