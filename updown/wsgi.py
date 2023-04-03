82446f9caf63e06c498ec3bd446ebf7855cf7fac
Kirk Northrop <kirk@krn.me.UK>
2023-04-03 13:28:14 +0100

    Add newrelic

--- updown/wsgi.py
+++ updown/wsgi.py
@@ -6,7 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
+import newrelic.agent
 
+newrelic.agent.initialize()
 import os
 
 from django.core.wsgi import get_wsgi_application
